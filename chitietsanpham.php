de46d0936529cf07a55190f26309814b1b73a5f7
BEEMO296 <chomchomviethoang@gmail.com>
2025-05-12 22:18:25 +0700

    Add files via upload

--- chitietsanpham.php
+++ chitietsanpham.php
@@ -1,59 +1,6 @@
-<!DOCTYPE html>
-<html lang="vi">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Chi Tiết Sản Phẩm</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="../css/index.css">
-</head>
-<body>
-
-<!-- Navbar include từ index -->
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">
-        <img src="../image/logo.png" > SHARK BookStore
-      </a>
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-        <span class="icon-bar" style="background:white;"></span>
-        <span class="icon-bar" style="background:white;"></span>
-        <span class="icon-bar" style="background:white;"></span>
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="navbar-collapse">
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Tìm kiếm sách...">
-        </div>
-        <button type="submit" class="btn btn-default">Tìm</button>
-      </form>
-
-   <!--ĐÂY LÀ CHỖ CHỌN THỂ LOẠI-->
-   <ul class="nav navbar-nav">
-    <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Thể loại <span class="caret"></span></a>
-      <ul class="dropdown-menu">
-        <li><a href="danhsachsp.html">Light Novel</a></li>
-                  <li><a href="danhsachsp.html">Manga</a></li>
-                  <li><a href="danhsachsp.html">Ngôn tình / Học đường</a></li>
-                  <li><a href="danhsachsp.html">Kinh dị</a></li>
-                  <li><a href="danhsachsp.html">Truyện thiếu nhi</a></li>
-                  <li><a href="danhsachsp.html">Phiêu lưu / Kỳ ảo</a></li>
-      </ul>
-    </li>
-  </ul>
-
-
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Giỏ hàng</a></li>
-        <li><a href="#">Đăng nhập</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
+<?php
+include_once 'header.php';
+?>
 <!-- Section -->
 <div class="container product-section">
   <div class="row">
