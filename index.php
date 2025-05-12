bb7e2d67dca1e16ef1fd04447c17f6b9b2f51822
BEEMO296 <chomchomviethoang@gmail.com>
2025-05-12 12:12:01 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,107 @@
 <?php 
 include_once 'header.php';
-include_once 'connect.php'; // file này chứa kết nối CSDL
-
-// Lấy sách mới nhất (giới hạn 8 cuốn)
-$sql = "SELECT s.MaSach, s.TenSach, s.GiaBan, ha.Ten_file_anh 
-        FROM sach s 
-        LEFT JOIN hinh_anh ha ON s.MaSach = ha.MaSach AND ha.TrangThai = 1 
-        ORDER BY s.NgayCapNhat DESC 
-        LIMIT 8";
-$result = mysqli_query($conn, $sql);
 ?>
 
 <div class="banner"></div>
 
+
 <div class="container">
   <h3 class="text-center">Sách Mới</h3>
   <div class="row">
-    <?php while($row = mysqli_fetch_assoc($result)) { ?>
-      <div class="col-sm-6 col-md-3">
+
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-default panel-book text-center">
+        <div class="panel-body">
+          <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+1" class="img-responsive center-block" alt="Sách 1">
+          <h4>Tên Sách 1</h4>
+          <p class="text-danger">100.000đ</p>
+          <button class="btn btn-buy">Mua ngay</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-default panel-book text-center">
+        <div class="panel-body">
+          <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+2" class="img-responsive center-block" alt="Sách 2">
+          <h4>Tên Sách 2</h4>
+          <p class="text-danger">120.000đ</p>
+          <button class="btn btn-buy">Mua ngay</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-default panel-book text-center">
+        <div class="panel-body">
+          <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+3" class="img-responsive center-block" alt="Sách 3">
+          <h4>Tên Sách 3</h4>
+          <p class="text-danger">90.000đ</p>
+          <button class="btn btn-buy">Mua ngay</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-6 col-md-3">
+      <div class="panel panel-default panel-book text-center">
+        <div class="panel-body">
+          <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+4" class="img-responsive center-block" alt="Sách 4">
+          <h4>Tên Sách 4</h4>
+          <p class="text-danger">110.000đ</p>
+          <button class="btn btn-buy">Mua ngay</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-6 col-md-3">
         <div class="panel panel-default panel-book text-center">
           <div class="panel-body">
-            <img src="images/<?php echo htmlspecialchars($row['Ten_file_anh'] ?? 'no-image.jpg'); ?>" 
-                 class="img-responsive center-block" 
-                 alt="<?php echo htmlspecialchars($row['TenSach']); ?>">
-            <h4><?php echo htmlspecialchars($row['TenSach']); ?></h4>
-            <p class="text-danger"><?php echo number_format($row['GiaBan'], 0, ',', '.') . 'đ'; ?></p>
+            <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+5" class="img-responsive center-block" alt="Sách 5">
+            <h4>Tên Sách 5</h4>
+            <p class="text-danger">110.000đ</p>
             <button class="btn btn-buy">Mua ngay</button>
-            <a href="chitiet.php?MaSach=<?php echo $row['MaSach']; ?>" class="btn btn-default">Xem chi tiết</a>
           </div>
         </div>
       </div>
-    <?php } ?>
+      
+      <div class="col-sm-6 col-md-3">
+        <div class="panel panel-default panel-book text-center">
+          <div class="panel-body">
+            <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+6" class="img-responsive center-block" alt="Sách 6">
+            <h4>Tên Sách 6</h4>
+            <p class="text-danger">110.000đ</p>
+            <button class="btn btn-buy">Mua ngay</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-6 col-md-3">
+        <div class="panel panel-default panel-book text-center">
+          <div class="panel-body">
+            <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+7" class="img-responsive center-block" alt="Sách 7">
+            <h4>Tên Sách 7</h4>
+            <p class="text-danger">110.000đ</p>
+            <button class="btn btn-buy">Mua ngay</button>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-sm-6 col-md-3">
+        <div class="panel panel-default panel-book text-center">
+          <div class="panel-body">
+            <img src="https://dummyimage.com/200x300/cccccc/000000&text=Sách+8" class="img-responsive center-block" alt="Sách 8">
+            <h4>Tên Sách 8</h4>
+            <p class="text-danger">110.000đ</p>
+            <button class="btn btn-buy">Mua ngay</button>
+          </div>
+        </div>
+      </div>
+
   </div>
 </div>
 
+<!-- Footer -->
 <?php 
 include_once 'footer.php';
 ?>
